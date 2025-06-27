2e03388f635b103e24ad040542cab9eefeffaa7b
Hernán Rago <hernanrago@msn.com>
2025-06-26 23:05:08 -0300

    First entities

---
+++ GasStation.cs
@@ -0,0 +1,16 @@
+using MinimalApiBase;
+
+public class GasStation : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double PricePerGallon { get; set; }
+    public DateTime LastUpdated { get; set; } = DateTime.UtcNow;
+
+    public override string ToString()
+    {
+        return $"{Name} - {Address} - ${PricePerGallon}/gallon";
+    }
+}
